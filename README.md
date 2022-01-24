d1e729fb06768f6784b52e0366890d36d1d1a3d1
Dan Lynch <pyramation@gmail.com>
2022-01-24 10:30:08 -0800

    fixtures

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # autosmosis
+
+## testing
+
+first start the tests
+
+```
+cd ./packages/autosmosis
+yarn test:watch
+```
+
+hit "p" and then type a search string to scope to your test, e.g., "keplr.bank"
---
+++ packages/autosmosis/__tests__/imperator.pools.test.js
@@ -0,0 +1,6 @@
+import pools from '../__fixtures__/imperator/search/v1/pools/data.json';
+describe('can read validator pool info', () => {
+    it ('pools', async () => {
+        console.log(pools)
+    })
+});
