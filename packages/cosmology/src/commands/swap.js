7864a1f1d7981c73c4a0bc83110fd495ace36f53
Dan Lynch <pyramation@gmail.com>
2022-03-31 00:28:20 -0700

    swap

--- packages/cosmology/src/commands/swap.js
+++ packages/cosmology/src/commands/swap.js
@@ -1,6 +1,4 @@
 import { chains } from '@cosmology/cosmos-registry';
-import { coin } from '@cosmjs/amino';
-
 import { assets } from '@cosmology/cosmos-registry';
 import { prompt } from '../utils';
 import { OsmosisApiClient } from '../clients/osmosis';
@@ -24,11 +22,9 @@ import {
 } from '../utils/osmo/utils';
 import { getSigningOsmosisClient, noDecimals } from '../messages/utils';
 import { messages } from '../messages/messages';
-import { signAndBroadcastTilTxExists } from '../messages/utils';
-import { Dec } from '@keplr-wallet/unit';
+import { signAndBroadcast } from '../messages/utils';
 
 const osmoChainConfig = chains.find((el) => el.chain_name === 'osmosis');
-// const restEndpoint = osmoChainConfig.apis.rest[0].address;
 const rpcEndpoint = osmoChainConfig.apis.rpc[0].address;
 
 const assetList = assets
@@ -216,9 +212,8 @@ export default async (argv) => {
 
   console.log(msg);
 
-  const res = await signAndBroadcastTilTxExists({
+  const res = await signAndBroadcast({
     client: stargateClient,
-    cosmos: client,
     chainId: osmoChainConfig.chain_id,
     address: osmoAddress,
     msg,
@@ -226,37 +221,6 @@ export default async (argv) => {
     memo: ''
   });
 
-  const block = res?.tx_response?.height;
-
-  // ERRORS
-
-  //   {
-  //     code: 7,
-  //     height: 3612102,
-  //     rawLog: 'failed to execute message; message index: 0: ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2 token is lesser than min amount: calculated amount is lesser than min amount',
-  //     transactionHash: '19AEB7F56FD26F205F986C4D7E5846CC821CDE941DB1215763C3DADE34FDBBA3',
-  //     gasUsed: 50716,
-  //     gasWanted: 250000
-  //   }
-
-  // SUCCESS
-
-  // broadcast
-  // {
-  //   code: 0,
-  //   height: 3612234,
-  //   rawLog: '[{"events"...]',
-  //   transactionHash: '2F184843E66278CBB3EAB2065E8E5F17B3D2913EB4AEDA2DB9E69843EB1B83C8',
-  //   gasUsed: 117782,
-  //   gasWanted: 250000
-  // }
-  // success at block 3612234
-
-  if (block) {
-    console.log(`success at block ${block}`);
-  } else {
-    console.log('no block found for tx!');
-  }
   console.log('\n\n\n\n\ntx');
   console.log(res);
 };
