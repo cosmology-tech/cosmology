abce1ccb2de815be27805db9576aec41226a0172
Dan Lynch <pyramation@gmail.com>
2022-04-01 01:33:56 -0700

    remove driver

--- packages/cosmology/__tests__/calcs.gecko.test.js
+++ packages/cosmology/__tests__/calcs.gecko.test.js
@@ -274,23 +274,3 @@ describe('user actions', () => {
     console.log(JSON.stringify(trades, null, 2));
   });
 });
-
-it('calcs', async () => {
-  // console.log(prices);
-  // console.log(bank);
-  // console.log(pools);
-  // let driver = new Driver()
-  // const jobs = await driver.getAllJobs([
-  //   { "type": "coin", "coin": "UST", "weight": 0.3 },
-  //   { "type": "pool", "pool": { "id": 562, "coin1": "LUNA", "coin2": "UST", "balance": 0.5 }, "weight": 0.3 },
-  //   { "type": "pool", "pool": { "id": 611, "coin1": "ATOM", "coin2": "STARS", "balance": 0.7 }, "weight": 0.4 }
-  // ])
-  // console.log(jobs);
-  // expect(jobs.length).toEqual(10)
-  // expect(jobs[0].job.inputCoin).toEqual("LUNA")
-  // expect(jobs[0].job.targetCoin).toEqual("UST")
-  // expect(jobs[0].job.amount).toBeGreaterThan(0)
-  // expect(jobs[5].job.inputCoin).toEqual("UST")
-  // expect(jobs[5].job.targetCoin).toEqual("STARS")
-  // expect(jobs[5].job.amount).toBeGreaterThan(0)
-});
--- packages/cosmology/src/index.js
+++ packages/cosmology/src/index.js
@@ -2,5 +2,4 @@ export * from './assets';
 export * from './clients';
 export * from './utils/osmo';
 export * from './utils/chain';
-export * from './driver/driver';
 export * from './messages';
