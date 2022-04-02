8d3ff0f8149ad2359c4b4935f38826e039903123
Dan Lynch <pyramation@gmail.com>
2022-04-02 08:27:24 -0700

    types

--- packages/cosmology/__fixtures__/clients/validator.js
+++ packages/cosmology/__fixtures__/clients/validator.js
@@ -4,68 +4,14 @@ import getPoolAprsFixture from '../validator/apr/v1/all/data.json';
 import getPoolAprFixture from '../validator/apr/v1/606/data.json';
 import getPairsSummaryFixture from '../validator/pairs/v1/summary/data.json';
 
-/**
- * 
- * @typedef {(
- * string
- * )} CoinDenom
- *
- * @typedef {( 
- * string
- * )} CoinSymbol
- * 
- * @typedef {{
- *  price: number;
- *  denom: CoinDenom;
- *  symbol: CoinSymbol;
- *  liquidity: number;
- *  liquidity_24h_change: number;
- *  volume_24h: number;
- *  volume_24h_change: number;
- *  name: string;
- * }} ValidatorToken
- * 
- * @typedef {{
- *  price: number;
- * }} ValidatorTokenPrice
- * 
- * @typedef {{
- * start_date: string;
- * denom: CoinDenom;
- * symbol: CoinSymbol;
- * apr_1d: number;
- * apr_7d: number;
- * apr_14d: number;
- * }} ValidatorCoinApr
- * 
- * @typedef {{
- *  pool_id: number;
- *  apr_list: ValidatorCoinApr[]
- * }} ValidatorPoolApr
- * 
- * 
- * interestingly this one has "price" which I think is the
- * price of the LP token? TODO: verify...
- * 
- * @typedef {{
- *  pool_address: string;
- *  pool_id: string;
- *  base_name: string;
- *  base_symbol: CoinSymbol;
- *  base_address: CoinDenom;
- *  quote_name: string;
- *  quote_symbol: CoinSymbol;
- *  quote_address: CoinDenom;
- *  price: number;
- *  base_volume_24h: number;
- *  quote_volume_24h: number;
- *  volume_24h: number;
- *  volume_7d: number;
- *  liquidity: number;
- *  liquidity_atom: number;
- * }} ValidatorPair
- * 
- */
+import {
+  ValidatorToken,
+  CoinSymbol,
+  ValidatorPoolApr,
+  ValidatorPair,
+  ValidatorTokenPrice
+} from '../../src/types';
+
 //  https://api-osmosis.imperator.co/swagger/#/
 export class OsmosisValidatorClient {
   async getPools() {
@@ -86,7 +32,7 @@ export class OsmosisValidatorClient {
   async getToken(symbol) {
     console.warn('NOT IMPLEMENTED IN TESTING');
   }
-  
+
   /**
    * @param {CoinSymbol} symbol
    * @returns {Promise<ValidatorTokenPrice[]>}
@@ -106,15 +52,14 @@ export class OsmosisValidatorClient {
    * @param {CoinSymbol} symbol
    * @returns {Promise<ValidatorPoolApr[]>}
    */
-   async getPoolApr(symbol) {
+  async getPoolApr(symbol) {
     return getPoolAprFixture;
-   }
+  }
 
   /**
    * @returns {Promise<ValidatorPair[]>}
    */
-   async getPairsSummary() {
+  async getPairsSummary() {
     return getPairsSummaryFixture;
-   }
-
+  }
 }
