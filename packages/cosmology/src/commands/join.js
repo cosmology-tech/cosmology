0a25276e2de0454b1288978ca2c3d6886ccd7f1e
Dan Lynch <pyramation@gmail.com>
2022-04-03 22:04:16 -0700

    join

--- packages/cosmology/src/commands/join.js
+++ packages/cosmology/src/commands/join.js
@@ -2,47 +2,47 @@ import { chains } from '@cosmology/cosmos-registry';
 import { coin } from '@cosmjs/amino';
 import { prompt } from '../utils';
 import { OsmosisApiClient } from '../clients/osmosis';
-import { OsmosisValidatorClient } from '../clients/validator';
 import { osmoRestClient } from '../utils';
 import {
   calculateShareOutAmount,
   calculateCoinsNeededInPoolForValue,
-  calculateMaxCoinsForPool
+  calculateMaxCoinsForPool,
+  makePoolsPretty,
+  makePoolsPrettyValues
 } from '../utils/osmo';
 import { getSigningOsmosisClient } from '../messages/utils';
 import { messages } from '../messages/messages';
 import { signAndBroadcast } from '../messages/utils';
-import { getPools } from '../utils/prompt';
 import { getPricesFromCoinGecko } from '../clients/coingecko';
-import {
-  printSwap,
-  printSwapForPoolAllocation,
-  printOsmoTransactionResponse
-} from '../utils/print';
+import { printOsmoTransactionResponse } from '../utils/print';
 
 const osmoChainConfig = chains.find((el) => el.chain_name === 'osmosis');
 const rpcEndpoint = osmoChainConfig.apis.rpc[0].address;
 
 export default async (argv) => {
-  const validator = new OsmosisValidatorClient();
   const api = new OsmosisApiClient();
+  const prices = await getPricesFromCoinGecko();
+  const lcdPools = await api.getPools();
+  const prettyPools = makePoolsPretty(prices, lcdPools.pools);
+  if (!argv['liquidity-limit']) argv['liquidity-limit'] = 100_000;
+  const poolListValues = makePoolsPrettyValues(
+    prettyPools,
+    argv['liquidity-limit']
+  );
+
   const { client, wallet: signer } = await osmoRestClient(argv);
   const [account] = await signer.getAccounts();
 
   const accountBalances = await client.getBalances(account.address);
   const balances = accountBalances.result;
-  // get pricing and pools info...
-  const prices = await getPricesFromCoinGecko();
-  //   const pools = await api.getPoolsPretty();
 
-  const poolList = await getPools(validator, argv);
   const { poolId } = await prompt(
     [
       {
         type: 'fuzzy:objects',
         name: 'poolId',
         message: 'choose pools to invest in',
-        choices: poolList
+        choices: poolListValues
       }
     ],
     argv
