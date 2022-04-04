422040841f5b3fe5a33e117844ae75b274cf41cc
Dan Lynch <pyramation@gmail.com>
2022-04-03 22:05:32 -0700

    cleanup

--- packages/cosmology/__tests__/osmosis.pools.test.js
+++ packages/cosmology/__tests__/osmosis.pools.test.js
@@ -1,8 +1,4 @@
 // @ts-nocheck
-import validatorPricesFixture from '../__fixtures__/validator/tokens/v2/all/data.json';
-import pairsFixture from '../__fixtures__/validator/pairs/v1/summary/data.json';
-import poolsFixture from '../__fixtures__/lcd/osmosis/gamm/v1beta1/pools/data.json';
-import lockedPoolsFixture from '../__fixtures__/lcd/osmosis/lockup/v1beta1/account_locked_coins/osmo1/data.json';
 import pricesFixture from '../__fixtures__/coingecko/api/v3/simple/price/data.json';
 
 import {
