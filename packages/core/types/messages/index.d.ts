a6f4ac55acbb2e3f383f8db64136746a569ff6ba
Dan Lynch <pyramation@gmail.com>
2022-04-15 16:50:10 -0700

    types

--- packages/core/types/messages/index.d.ts
+++ packages/core/types/messages/index.d.ts
@@ -2,5 +2,5 @@ export * from './meta';
 export * from './aminos';
 export * from './utils';
 export declare const messages: any;
-import { osmosis } from '../proto/generated/codecimpl';
-export { osmosis };
+import * as telescope from '@cosmonauts/osmosis';
+export { telescope };
--- packages/core/types/messages/native.d.ts
+++ packages/core/types/messages/native.d.ts
@@ -1,1 +1,66 @@
-export declare const native: any;
+export declare const native: {
+    transfer(value: import("@cosmonauts/osmosis/types/proto/ibc/applications/transfer/v1/tx").MsgTransfer): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/ibc/applications/transfer/v1/tx").MsgTransfer;
+    };
+    send(value: import("@cosmonauts/osmosis/types/proto/cosmos/bank/v1beta1/tx").MsgSend): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/bank/v1beta1/tx").MsgSend;
+    };
+    multiSend(value: import("@cosmonauts/osmosis/types/proto/cosmos/bank/v1beta1/tx").MsgMultiSend): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/bank/v1beta1/tx").MsgMultiSend;
+    };
+    submitProposal(value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgSubmitProposal): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgSubmitProposal;
+    };
+    vote(value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgVote): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgVote;
+    };
+    voteWeighted(value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgVoteWeighted): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgVoteWeighted;
+    };
+    deposit(value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgDeposit): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/gov/v1beta1/tx").MsgDeposit;
+    };
+    setWithdrawAddress(value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgSetWithdrawAddress): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgSetWithdrawAddress;
+    };
+    withdrawDelegatorReward(value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgWithdrawDelegatorReward): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgWithdrawDelegatorReward;
+    };
+    withdrawValidatorCommission(value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgWithdrawValidatorCommission): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgWithdrawValidatorCommission;
+    };
+    fundCommunityPool(value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgFundCommunityPool): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/distribution/v1beta1/tx").MsgFundCommunityPool;
+    };
+    createValidator(value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgCreateValidator): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgCreateValidator;
+    };
+    editValidator(value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgEditValidator): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgEditValidator;
+    };
+    delegate(value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgDelegate): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgDelegate;
+    };
+    beginRedelegate(value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgBeginRedelegate): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgBeginRedelegate;
+    };
+    undelegate(value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgUndelegate): {
+        typeUrl: string;
+        value: import("@cosmonauts/osmosis/types/proto/cosmos/staking/v1beta1/tx").MsgUndelegate;
+    };
+};
