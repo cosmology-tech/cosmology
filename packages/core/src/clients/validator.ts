7f837866ddbdc71e0b541b5caede3217461c3da1
njerschow <nj18@nyu.edu>
2022-04-22 18:33:07 +0200

    fix validator calls

--- packages/core/src/clients/validator.ts
+++ packages/core/src/clients/validator.ts
@@ -1,5 +1,5 @@
-import { RestClient } from './rest';
-import autobind from 'class-autobind';
+import { RestClient } from './rest'
+import autobind from 'class-autobind'
 
 import {
   ValidatorToken,
@@ -7,48 +7,48 @@ import {
   ValidatorPoolApr,
   ValidatorPair,
   ValidatorTokenPrice
-} from '../types';
+} from '../types'
 
 //  https://api-osmosis.imperator.co/swagger/#/
 export class OsmosisValidatorClient extends RestClient {
-  constructor({ url = 'https://api-osmosis.imperator.co/' } = {}) {
-    super({ url });
-    this._clientType = 'Osmosis Validator';
-    autobind(this); // React ES6 doesn't bind this -> meaning we get 'unable to read property 'request' of undefined
+  constructor ({ url = 'https://api-osmosis.imperator.co/' } = {}) {
+    super({ url })
+    // this._clientType = 'Osmosis Validator';
+    autobind(this) // React ES6 doesn't bind this -> meaning we get 'unable to read property 'request' of undefined
   }
 
-  async getPools() {
-    const endpoint = `search/v1/pools`;
-    return await this.request(endpoint);
+  async getPools () {
+    const endpoint = `search/v1/pools`
+    return await this.request(endpoint)
   }
 
-  async getTokens(): Promise<ValidatorToken[]> {
-    const endpoint = `tokens/v2/all`;
-    return await this.request(endpoint);
+  async getTokens (): Promise<ValidatorToken[]> {
+    const endpoint = `tokens/v2/all`
+    return await this.request(endpoint)
   }
 
-  async getToken(symbol: CoinSymbol): Promise<ValidatorToken[]> {
-    const endpoint = `tokens/v2/${symbol}`;
-    return await this.request(endpoint);
+  async getToken (symbol: CoinSymbol): Promise<ValidatorToken[]> {
+    const endpoint = `tokens/v2/${symbol}`
+    return await this.request(endpoint)
   }
 
-  async getTokenPrice(symbol: CoinSymbol): Promise<ValidatorTokenPrice[]> {
-    const endpoint = `tokens/v2/price/${symbol}`;
-    return await this.request(endpoint);
+  async getTokenPrice (symbol: CoinSymbol): Promise<ValidatorTokenPrice[]> {
+    const endpoint = `tokens/v2/price/${symbol}`
+    return await this.request(endpoint)
   }
 
-  async getPoolAprs(): Promise<ValidatorPoolApr[]> {
-    const endpoint = `apr/v2/all`;
-    return await this.request(endpoint);
+  async getPoolAprs (): Promise<ValidatorPoolApr[]> {
+    const endpoint = `apr/v2/all`
+    return await this.request(endpoint)
   }
 
-  async getPoolApr(symbol: CoinSymbol): Promise<ValidatorPoolApr[]> {
-    const endpoint = `apr/v2/${symbol}`;
-    return await this.request(endpoint);
+  async getPoolApr (symbol: CoinSymbol): Promise<ValidatorPoolApr[]> {
+    const endpoint = `apr/v2/${symbol}`
+    return await this.request(endpoint)
   }
 
-  async getPairsSummary(): Promise<ValidatorPair[]> {
-    const endpoint = `pairs/v1/summary`;
-    return await this.request(endpoint);
+  async getPairsSummary (): Promise<ValidatorPair[]> {
+    const endpoint = `pairs/v1/summary`
+    return await this.request(endpoint)
   }
 }
