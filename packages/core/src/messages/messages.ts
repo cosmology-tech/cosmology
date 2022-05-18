5583f88121d97e14cbf45e91b90851d2ba238f31
Dan Lynch <pyramation@gmail.com>
2022-05-17 22:16:20 -0700

    native

--- packages/core/src/messages/messages.ts
+++ packages/core/src/messages/messages.ts
@@ -1,7 +1,11 @@
-import { osmosis } from 'osmojs';
+import { osmosis, cosmos } from 'osmojs';
 
 export const messages = {
   ...osmosis.gamm.v1beta1.MessageComposer.withTypeUrl,
   ...osmosis.superfluid.MessageComposer.withTypeUrl,
-  ...osmosis.lockup.MessageComposer.withTypeUrl
+  ...osmosis.lockup.MessageComposer.withTypeUrl,
+  // native
+  ...cosmos.distribution.v1beta1.MessageComposer.fromPartial,
+  ...cosmos.bank.v1beta1.MessageComposer.fromPartial,
+  ...cosmos.staking.v1beta1.MessageComposer.fromPartial
 };
