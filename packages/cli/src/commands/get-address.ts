0abb9f24967b603a78e60125594b1b84df1cd55c
Dan Lynch <pyramation@gmail.com>
2022-09-11 16:21:07 -0400

    address

--- packages/cli/src/commands/get-address.ts
+++ packages/cli/src/commands/get-address.ts
@@ -1,7 +1,7 @@
-import { promptOsmoWallet } from '../utils';
+import { promptWalletOfToken } from '../utils';
 
 export default async (argv) => {
-    const signer = await promptOsmoWallet(argv);
+    const signer = await promptWalletOfToken(argv);
     const [account] = await signer.getAccounts();
     console.log(account.address)
 };
