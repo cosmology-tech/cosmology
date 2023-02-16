6be4fa7e87ef2f37b83759ea7a865da48bd52e5c
aidanaden <aidanaden@hotmail.com>
2023-02-16 14:01:56 +0800

    feat: update pools query pagination limit to 2000

--- packages/core/src/clients/osmosis.ts
+++ packages/core/src/clients/osmosis.ts
@@ -31,7 +31,7 @@ export class OsmosisApiClient extends CosmosApiClient {
   }
 
   async getPools(): Promise<PoolsResponse> {
-    const endpoint = `osmosis/gamm/v1beta1/pools?pagination.limit=1000`;
+    const endpoint = `osmosis/gamm/v1beta1/pools?pagination.limit=2000`;
     return await this.request(endpoint);
   }
 
